7e899ab822fdae47999222b2b2e32ec756c0c6e6
deknusapt <dev.deknusa@gmail.com>
2024-11-06 07:58:14 +0800

    feat: membuat class dan object.

---
+++ 01.class_object/produk.php
@@ -0,0 +1,14 @@
+<?php
+
+// Penulisan class{} pada php, merupakan template/blueprint
+class Produk
+{
+
+}
+
+/* Inisiasi sebuah instance yang merupakan
+ * bentuk nyata dari sebuah class.
+ */
+
+$produk1 = new Produk();
+$produk1 = new Produk();
